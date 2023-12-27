79d3622fdb1fa4567852ae0def54f35ebc0a7876
Phillip Hall <talvor@me.com>
2023-12-27 21:54:11 +1100

    Add treesitter nvim-tree lualine to neovim

--- home-manager/programs/neovim/default.nix
+++ home-manager/programs/neovim/default.nix
@@ -3,8 +3,47 @@
 {
     programs.neovim = {
         enable = true;
-        extraConfig = ''
-            set number relativenumber
+        extraLuaConfig = ''
+            ${builtins.readFile config/mappings.lua}
+            ${builtins.readFile config/options.lua}
         '';
+        plugins = [
+            ## Theme
+            {
+                plugin = pkgs.vimPlugins.nordic-nvim;
+                config = "vim.cmd[[colorscheme nordic]]";
+                type = "lua";
+            }
+            ## Lualine
+            {
+                plugin = pkgs.vimPlugins.lualine-nvim;
+                config = ''
+                    require('lualine').setup {
+                        options = {
+                            theme = 'nord',
+                        }
+                    }
+                '';
+                type = "lua";
+            }
+            ## nvim-tree
+            {
+                plugin = pkgs.vimPlugins.nvim-tree-lua;
+                type = "lua";
+                config = ''
+                    require("nvim-tree").setup()
+                '';
+            }
+            pkgs.vimPlugins.nvim-web-devicons
+
+            ## Treesitter
+            {
+                plugin = pkgs.vimPlugins.nvim-treesitter;
+                config = builtins.readFile config/setup/treesitter.lua;
+                type = "lua";
+            }
+            pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+            pkgs.vimPlugins.nvim-treesitter-textobjects
+        ];
     };
 }
