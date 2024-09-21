11fa73af1e502830ba7bb4c8fa0b72d6dfe2b746
Phillip Hall <talvor@me.com>
2024-09-21 20:51:16 +1000

    remove nixvim config

--- configs/users/phillip/configs/nixvim/plugins/ui/neo-tree.nix
+++
@@ -1,1 +0,0 @@
-{ programs.nixvim.plugins.neo-tree = { enable = true; }; }
--- configs/users/phillip/home.nix
+++ configs/users/phillip/home.nix
@@ -10,7 +10,6 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    ./configs/nixvim/nixvim.nix
     ./configs/tmux.nix
     ./configs/zsh.nix
   ];
